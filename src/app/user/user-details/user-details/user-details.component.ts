b3000cca9b166cf4e5a11ee1961e448598438a8e
Ruymán <ruyman.padronc@gmail.com>
2020-07-16 11:18:41 +0100

    Add user details view

--- src/app/user/user-details/user-details/user-details.component.ts
+++ src/app/user/user-details/user-details/user-details.component.ts
@@ -1,8 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../user.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Professional } from 'src/app/models/professional.model';
 import { Patient } from 'src/app/models/patient.model';
+import { MatDialog } from '@angular/material/dialog';
+import { User } from 'src/app/models/user.model';
+import { DialogComponent } from 'src/app/dialog/dialog/dialog.component';
+
+enum type {
+  patient,
+  professional
+}
 
 @Component({
   selector: 'app-user-details',
@@ -11,18 +19,39 @@ import { Patient } from 'src/app/models/patient.model';
 })
 export class UserDetailsComponent implements OnInit {
   user: Patient | Professional
+  userType: type
 
-  constructor(private userService: UserService, private route: ActivatedRoute) { }
+  constructor(private userService: UserService, private route: ActivatedRoute, private router: Router, private dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       if (params.id) {
         this.userService.getUser(params.id)
           .subscribe(user => {
-            this.user = user;
+            if(user.hasOwnProperty('NHC')) this.userType = type.patient
+            else this.userType = type.professional
+            this.user = user
           });
       }
     });
+  }
+
+  deleteUser(row: User): void {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      width: '350px',
+      data: { title: 'Eliminar ' + row.name, body: '¿Está seguro de que desea eliminar el usuario?' }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.userService.deleteUser(row.id).subscribe(() => {
+          this.router.navigate(['/'])
+        });
+      }
+    });
+  }
+
+  editUser(row: User): void {
+    this.router.navigate(['/users/edit/' + row.id])
   }
 
 }
