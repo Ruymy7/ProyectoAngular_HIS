0df477f461a32ce40f66141ee1600af0ec176798
Ruymán <ruyman.padronc@gmail.com>
2020-07-23 08:49:41 +0100

    Change typing of Patients and Professionals in user service

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { Patient } from '../models/patient.model'
 import { Professional } from '../models/professional.model'
 
+type User = Patient | Professional
 
 @Injectable({
   providedIn: 'root'
@@ -12,20 +13,20 @@ import { Professional } from '../models/professional.model'
 export class UserService {
   constructor(private httpClient: HttpClient) { }
 
-  getAllUsers(): Observable<Patient[] | Professional[]> {
-    return this.httpClient.get<Patient[] | Professional[]>(environment.API_URL + 'users')
+  getAllUsers(): Observable<User[]> {
+    return this.httpClient.get<User[]>(environment.API_URL + 'users')
   }
-  getUser(id: string): Observable<Patient | Professional> {
-    return this.httpClient.get<Patient | Professional>(environment.API_URL + `users/${id}`)
+  getUser(id: string): Observable<User> {
+    return this.httpClient.get<User>(environment.API_URL + `users/${id}`)
   }
-  addUser(userData: Patient | Professional): Observable<Patient | Professional> {
-    return this.httpClient.post<Patient | Professional>(environment.API_URL + 'users', userData)
+  addUser(userData: User): Observable<User> {
+    return this.httpClient.post<User>(environment.API_URL + 'users', userData)
   }
-  editUser(userData: Patient | Professional): Observable<Patient | Professional> {
-    return this.httpClient.put<Patient | Professional>(environment.API_URL + `users/${userData.id}`, userData)
+  editUser(userData: User): Observable<User> {
+    return this.httpClient.put<User>(environment.API_URL + `users/${userData.id}`, userData)
   }
-  deleteUser(id: string): Observable<Patient | Professional> {
-    return this.httpClient.delete<Patient | Professional>(environment.API_URL + `users/${id}`)
+  deleteUser(id: string): Observable<User> {
+    return this.httpClient.delete<User>(environment.API_URL + `users/${id}`)
   }
   deleteUsers(ids: string[]): Promise<object> { // This is a solution if the backend doesn't support multiple deletion
     let promiseArray:Promise<object>[] = []
