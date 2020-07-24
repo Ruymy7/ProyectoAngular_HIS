06dad449f596f9e5de2673829d86f941c1431cc8
Ruymán <ruyman.padronc@gmail.com>
2020-07-24 12:45:38 +0100

    Move server.js to project root folder

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(__dirname+'/dist/'));
-app.get('/',function(req,res){
+app.get('/*',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
