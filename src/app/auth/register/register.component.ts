2163c4f15c75f95bb74e30cc9e605c5a69a3ed70
Ruymán <ruyman.padronc@gmail.com>
2020-07-29 09:36:43 +0100

    Register form validations

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,28 +1,55 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent  {
-  name: string
-  email: string
-  password: string
-  confirmPassword: string
+export class RegisterComponent {
 
-  constructor(private authService: AuthService, private router: Router) { }
+  registerForm: FormGroup
+  userExisting: boolean = false
+  saved: boolean = false
+  error: boolean = false
+  
+  constructor(private authService: AuthService, private router: Router, private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    }, { validators: this.checkPasswords })
+  }
 
   register() {
-    if(this.password === this.confirmPassword) {
-      const registerUserDto = {name: this.name, email: this.email, password: this.password}
-      this.authService.register(registerUserDto).subscribe(response => {
-        this.router.navigate(['/login'])
+    console.log(this.registerForm.hasError('notSamePassword'));
+    this.userExisting = false; this.error = false; this.saved = false
+    if (this.registerForm.valid) {
+      this.authService.register(this.registerForm.value).subscribe(result => {
+        this.saved = true
+        setTimeout(() => {
+          this.saved = false
+          this.router.navigate(['/login'])
+        }, 2000)
+      }, error => {
+        if(error.status === 409) this.userExisting = true
+        else this.error = true
       })
     }
-    
   }
+
+  checkPasswords(form: FormGroup) {
+    return form.get('password').value === form.get('confirmPassword').value ? null : { notSamePassword: true }
+  }
+
+  get name() { return this.registerForm.get('name') }
+  get email() { return this.registerForm.get('email') }
+  get password() { return this.registerForm.get('password') }
+  get confirmPassword() { return this.registerForm.get('confirmPassword') }
 
 }
