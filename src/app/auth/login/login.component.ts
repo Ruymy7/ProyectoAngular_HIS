b56beb693bc2c7ad45464d858704a7899c26092d
Ruymán <ruyman.padronc@gmail.com>
2020-07-29 08:32:18 +0100

    Login form validators

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from '../auth.service';
 import { LogInResponseDto } from '../dto/log-in-response-dto';
 import { StorageService } from '../storage.service';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -11,20 +12,29 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
 
-  email: string
-  password: string
+  loginForm: FormGroup
 
-  constructor(private authService: AuthService, private storageService: StorageService, private router: Router) { }
+  constructor(private authService: AuthService, private storageService: StorageService, private router: Router, private fb: FormBuilder) { }
 
-  logIn() {
-    const logInDto = {email: this.email, password: this.password}
-    this.authService.logIn(logInDto).subscribe((response: LogInResponseDto) => {
-      if(response.status) {
-        this.storageService.setCurrentSession(response.data)
-        this.router.navigate(['/'])
-      }
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
     })
   }
 
+  logIn() {
+    if(this.loginForm.valid) {
+      this.authService.logIn(this.loginForm.value).subscribe((response: LogInResponseDto) => {
+        if(response.status) {
+          this.storageService.setCurrentSession(response.data)
+          this.router.navigate(['/'])
+        }
+      })
+    }
+  }
 
+
+  get email() { return this.loginForm.get('email') }
+  get password() { return this.loginForm.get('password') }
 }
